362ecdaf5f212bc0988c7b65a2ef8e9669d92200
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2023-11-03 21:19:53 +0500

    feat(code-preview): added a table to the render that is formed from the input data
    render method fills the table with code line by line with the number of each line

--- src/components/code-preview/code-preview.tsx
+++ src/components/code-preview/code-preview.tsx
@@ -1,17 +1,28 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Prop, h } from '@stencil/core';
 
 @Component({
   tag: 'code-preview',
   styleUrl: 'code-preview.scss',
-  shadow: true,
 })
 export class CodePreview {
+  @Prop()
+  code: string[];
 
+  /**
+   * @returns A table strings line by line.
+   * Uses {index + 1} to display the line number.
+   */
   render() {
+    if (!this.code || this.code.length === 0) {
+      return null;
+    }
+
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <tbody>
+        {this.code.map((row: string, index: number) => <tr>
+          <td>{index + 1}</td>  <td>{row}</td>
+        </tr>)}
+      </tbody>
     );
   }
 
