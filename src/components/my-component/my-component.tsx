f6662315c05a0c6d48796f1d2ebcfc2aa1824d9e
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2023-11-23 06:36:33 +0500

    refactor(single-file-view, metadata-header, svg): move all SVGs to separate file

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,5 @@
 import { Component, Listen, State, h } from '@stencil/core';
+import { DUMMY_CODE, DUMMY_GISTS_LIST } from '../../utils/utils';
 
 @Component({
   tag: 'my-component',
@@ -6,7 +7,41 @@ import { Component, Listen, State, h } from '@stencil/core';
   shadow: true,
 })
 export class MyComponent {
+
+  // @State()
+  // currentPage: number = 1;
+
+  // @State()
+  // isListLoading = true;
+
+  // @Listen('goToPage')
+  // TEST_goToPage(event: CustomEvent<number>) {
+  //   this.currentPage = event.detail;
+  //   console.log(this.currentPage);
+  // }
+
+  // @Listen('gistSelected')
+  // TEST_gistSelectedLog(event: CustomEvent<string>) {
+  //   console.log('gist choosed id: ', event.detail)
+  // }
+
+  // get lastPage(): boolean {
+  //   return this.currentPage >= 13;
+  // }
+
+  // componentWillLoad() {
+  //   setTimeout(() => this.isListLoading = false, 2000);
+  // }
+
   render() {
-    return <single-file-view filename='promisify.js' />
+    // return <gists-list
+    //   gistsList={DUMMY_GISTS_LIST}
+    //   currentPage={this.currentPage}
+    //   lastPage={this.lastPage}
+    // />
+    return <single-file-view
+      code={DUMMY_CODE}
+      filename='promisify.js'
+    />
   }
 }
